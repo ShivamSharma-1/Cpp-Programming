7a773cb908916209b3fc1b085dbd264efc9a34af
ShivamSharma-1 <shivamsharma.github@gmail.com>
2026-08-27 18:45:48 +0530

    Datatypes

---
+++ 04 - VARIABLES and DATATYPES/01_dataTypes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main() {
+    int age;
+    float cgpa;
+    double decimal;
+    char ch;
+    bool boy;
+
+    /*
+        THERE IS NOTHING IS STORED IN THESE VARIABLES
+        SO IT WILL COUT SOME GARBAGE VALUES...
+    */
+    
+    std :: cout << age << std :: endl;
+    std :: cout << cgpa << std :: endl;
+    std :: cout << decimal << std :: endl;
+    std :: cout << ch << std :: endl;
+    std :: cout << boy << std :: endl;
+
+    return 0;
+}
