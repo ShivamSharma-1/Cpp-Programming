4e22bbe660889b002b4340fbcaba09d7c0a6b731
ShivamSharma-1 <shivamsharma.github@gmail.com>
2026-09-22 23:07:45 +0530

    Convert to Uppercase

---
+++ 06 - OUTPUT FORMATTING/08_uppercase.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ios>
+
+int main() {
+
+    int posNum {717171};
+
+    std :: cout << "-----Default positive number in lowercase-----" << std :: endl;
+    std :: cout << "posNum in decimal : " << std :: dec << posNum << std :: endl;
+    std :: cout << "posNum in hexagonal : " << std :: hex << posNum << std :: endl;
+    std :: cout << "posNum in octal : " << std :: oct << posNum << std :: endl;
+
+    std :: cout << std :: endl;
+
+    std :: cout << "-----Positive number in uppercase-----" << std :: endl;
+    std :: cout << std :: uppercase;
+    std :: cout << "posNum in decimal : " << std :: dec << posNum << std :: endl;
+    std :: cout << "posNum in hexagonal : " << std :: hex << posNum << std :: endl;
+    std :: cout << "posNum in octal : " << std :: oct << posNum << std :: endl;
+}
